4ae3f0c930ca4877005ea588a2953ca48f4ed1be
mariia-ty <tymoshenkom@icloud.com>
2024-11-22 23:49:16 -0500

    started tasks

---
+++ index.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    name: "Stephanie Brown",
+    balance: 34567,
+    friends: 14,
+    skills: ["painting", "drawing", "befrending animals"],
+  },
+  {
+    name: "Bruce Wayne",
+    balance: 3450607,
+    friends: 4,
+    skills: ["skating", "leading", "finding things"],
+  },
+  {
+    name: "Richard Grayson",
+    balance: 285369,
+    friends: 17,
+    skills: ["gymnastics", "befrending animals", "acrobatics"],
+  },
+  {
+    name: "Jason Todd",
+    balance: 277,
+    friends: 8,
+    skills: ["gymnastics", "creative writing", "making rings"],
+  },
+  {
+    name: "Selina Kyle",
+    balance: 7083,
+    friends: 0,
+    skills: ["dancing", "skating", "drawing"],
+  },
+];
+
+//assign 1 total balance
+
+//assign 2 names of those who have friends
+
+//assign 3 names ordered by num of friends
+
+//assign 4 list of all skills, no repeats
